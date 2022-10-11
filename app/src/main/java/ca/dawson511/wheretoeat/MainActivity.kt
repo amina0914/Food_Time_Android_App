0f139122f40e71136ef96dbebaea130434670e06
amina.turd <aminaturdalieva@gmail.com>
2022-10-11 15:11:30 -0400

    added more info intent to search on a given food

--- app/src/main/java/ca/dawson511/wheretoeat/MainActivity.kt
+++ app/src/main/java/ca/dawson511/wheretoeat/MainActivity.kt
@@ -4,14 +4,22 @@ package ca.dawson511.wheretoeat
 //import android.R
 //import android.R
 import android.content.ContentValues.TAG
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import ca.dawson511.wheretoeat.databinding.ActivityMainBinding
 import kotlin.random.Random
+import android.Manifest
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val infoObj = object {
+        val LETTER = "letter"
+        val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
 
     private lateinit var binding: ActivityMainBinding
 
@@ -23,11 +31,20 @@ class MainActivity : AppCompatActivity() {
         val tryAgainButton = binding.tryAgain
         tryAgainButton.setOnClickListener { getRandomFood() }
 
-        getRandomFood()
+        val randomFood = getRandomFood()
+
+//        binding.foodImage.setOnClickListener { getMap() }
+
+
+        binding.searchButton.setOnClickListener {
+            if (randomFood != null) {
+                getSearch(randomFood)
+            }
+        }
     }
 
        // Function that chooses random food from list and updates the text and image.
-        fun getRandomFood(){
+        fun getRandomFood(): String? {
             val foods = this.resources.getStringArray(R.array.foods)
             val randomIndex = Random.nextInt(foods.size)
             val randomFood = foods[randomIndex]
@@ -37,7 +54,24 @@ class MainActivity : AppCompatActivity() {
            val imageId = resources.getIdentifier(randomFood.lowercase(), "drawable", packageName)
            binding.foodImage.setImageResource(imageId)
 
+           return randomFood
+
        }
+
+        fun getMap(geoLocation: Uri) {
+            val intent = Intent(Intent.ACTION_VIEW).apply {
+                data = geoLocation
+            }
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            }
+        }
+
+        fun getSearch(randomFood : String) {
+            val queryUrl: Uri = Uri.parse("${infoObj.SEARCH_PREFIX}${randomFood}")
+            val intent = Intent(Intent.ACTION_VIEW, queryUrl)
+            startActivity(intent)
+        }
 
 
 }
