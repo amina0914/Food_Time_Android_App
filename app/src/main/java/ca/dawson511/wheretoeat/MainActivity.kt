1f41ab784de99548f8382da887ee2138acb6617b
amina.turd <aminaturdalieva@gmail.com>
2022-10-11 01:18:55 -0400

    started random food function

--- app/src/main/java/ca/dawson511/wheretoeat/MainActivity.kt
+++ app/src/main/java/ca/dawson511/wheretoeat/MainActivity.kt
@@ -1,9 +1,12 @@
 package ca.dawson511.wheretoeat
 
 
+import android.R
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import ca.dawson511.wheretoeat.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +18,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-      }
+//        binding.tryAgain.setOnClickListener(getRandomFood())
+    }
+
+        //Function that chooses random food from list and updates the text and image.
+//        fun getRandomFood(){
+//            val foods = this.resources.getStringArray(R.array.foods)
+//            val randomIndex = Random.nextInt(foods.size)
+//            val randomFood = foods[randomIndex]
+//      }
+
+
 }
